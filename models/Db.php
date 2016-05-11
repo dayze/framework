7493c3f5f87759fcb05e3b1b7ffa3669849df8a2
Maxime Laine <maxime.laine@dsinstruments.fr>
2016-05-11 16:45:19 +0200

    update of abstract class for database connection

---
+++ models/Db.php
@@ -0,0 +1,22 @@
+<?php
+
+class Db
+{
+    private $instance;
+
+    public function __construct()
+    {
+        try {
+            $this->instance = new PDO(PDO_DSN, DB_LOGIN, DB_PASSWORD);
+        } catch (PDOException $e) {
+            throw new PDOException($e->getMessage());
+        }
+    }
+
+    public function __destruct()
+    {
+        $this->instance = null;
+    }
+
+
+}
